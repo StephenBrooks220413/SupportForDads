03364e35e0f85d3c9da3f8f98f7d6d0298265c3c
Stephen A Brooks <krammer439298@gmail.com>
2021-08-05 12:57:21 -0700

    Add project files.

---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SupportForDads.MvcApplication" Language="C#" %>
---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SupportForDads.Startup))]
+namespace SupportForDads
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
